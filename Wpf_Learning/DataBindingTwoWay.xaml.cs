a591918fcb5c3699e96baabf19bd364814d3f5b9
Longoria.You <longoria.you@gmail.com>
2017-10-22 15:06:42 +0800

    Two way binding learning

---
+++ Wpf_Learning/DataBindingTwoWay.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Wpf_Learning.ViewModel;
+
+namespace Wpf_Learning
+{
+    /// <summary>
+    /// Interaction logic for DataBindingTwoWay.xaml
+    /// </summary>
+    public partial class DataBindingTwoWay : Window
+    {
+        public DataBindingTwoWay()
+        {
+            InitializeComponent();
+            this.DataContext = Employee.GetEmployee();
+        }
+    }
+}
